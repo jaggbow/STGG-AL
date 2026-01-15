9f2b1b43bfb535e6fa2715de42149e845ba52c90
Oussama Boussif <oussama.boussif@umontreal.ca>
2026-01-15 08:08:07 -0800

    updated filtering prop script

--- src/experiments/filter_prop.sh
+++ src/experiments/filter_prop.sh
@@ -3,13 +3,12 @@
 #SBATCH --cpus-per-task=6
 #SBATCH --gpus=nvidia_h100_80gb_hbm3_3g.40gb:1
 #SBATCH --mem=48G
-#SBATCH --time=12:00:00
+#SBATCH --time=2:00:00
 #SBATCH -o /scratch/jaggbow/slurm-%j.out
 
 module load python/3.10
 module load xtb
 
-GENERATOR_CHECKPOINT_DIR=$SCRATCH/AutoregressiveMolecules_checkpoints/main_exp
 smiles_path=$(ls -t "$GENERATOR_CHECKPOINT_DIR"/*.pkl 2>/dev/null | grep -E '/[0-9]+\.pkl$' | head -n 1)
 
 
@@ -17,16 +16,16 @@ smiles_path=$(ls -t "$GENERATOR_CHECKPOINT_DIR"/*.pkl 2>/dev/null | grep -E '/[0
 cd $GENERATOR_DIR
 source .venv/bin/activate
 cd src
-python compute_rdkit_coordinates.py --smiles_path=$smiles_path
+python compute_rdkit_coordinates.py --smiles_path=$smiles_path --num_workers=6
 deactivate
 
 # Run property prediction
 cd $PROPERTY_PREDICTOR_DIR
 coordinate_dir=$(ls -td $GENERATOR_CHECKPOINT_DIR/*/ | head -n1)
 smiles_path=$(ls -t "$GENERATOR_CHECKPOINT_DIR"/*.pkl 2>/dev/null | grep -E '/[0-9]+\.pkl$' | head -n 1)
-csv_path="${smiles_path%.pkl}.csv"
+csv_path="${smiles_path%.pkl}_filtering.csv"
 echo $coordinate_dir,$smiles_path,$csv_path
-uv run predict.py --checkpoint_path="${PROPERTY_PREDICTOR_DIR}/results/STGGDataset/filtering/1/0/last.ckpt" --xtb_results_folder=$coordinate_dir --output_path=$csv_path
+uv run predict.py --source_csv_path=$csv_path --checkpoint_path="${PROPERTY_PREDICTOR_DIR}/results/STGGDataset/filtering/1/0/last.ckpt" --xtb_results_folder=$coordinate_dir --output_path=$csv_path
 deactivate
 
 # Filtering
